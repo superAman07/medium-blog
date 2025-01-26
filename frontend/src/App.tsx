eed57eccf3847dfe5df87f58c3ec8e9616fd7f51
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2025-01-26 19:14:02 +0000

    fixed an issue related with route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,38 @@
-// import './App.css'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
+import { Blog } from "./pages/Blog";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Signup } from "./pages/Signup" 
-import { Blog } from "./pages/Blog"
-import { Signin } from "./pages/Signin"
-import { Blogs } from "./pages/Blogs"
-import { Publish } from "./pages/Publish"
-
-function App() { 
+function App() {
+  const isAuthenticated = !!localStorage.getItem("token");  
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path = '/signup' element={<Signup/>}></Route>    
-        <Route path = '/signin' element={<Signin/>}></Route>    
-        <Route path = '/blogs' element={<Blogs/>}></Route>    
-        <Route path = '/' element={<Blogs/>}></Route>    
-        <Route path = '/blog/:id' element={<Blog/>}></Route>    
-        <Route path = '/publish' element={<Publish/>}></Route>    
+      <Routes> 
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+ 
+        <Route
+          path="/blogs"
+          element={isAuthenticated ? <Blogs /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/blog/:id"
+          element={isAuthenticated ? <Blog /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/publish"
+          element={isAuthenticated ? <Publish /> : <Navigate to="/signin" />}
+        />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Navigate to="/blogs" /> : <Navigate to="/signin" />}
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
